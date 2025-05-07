607c88bd311ba434aaab76f58f51b1562a5ebf6d
aroario2003 <borkbork1031@gmail.com>
2025-05-07 15:08:13 -0400

    add the CR_k gate to quantum circuit

--- quantum/qc.d
+++ quantum/qc.d
@@ -79,6 +79,24 @@ struct QuantumCircuit {
         }
     }
 
+    /// The controlled hadamard gate applies a hadamard transformation to the target qubit when the 
+    /// control qubit is in the state |1>
+    void ch(int control_qubit_idx, int target_qubit_idx) {
+        assert(this.num_qubits >= 2, "The number of qubits must be greater than or equal to two in order to use controlled gates");
+        for (int i = 0; i < this.state.length(); i++) {
+            bool cntl_qubit_is_one = (i & (1 << control_qubit_idx)) != 0;
+            if (cntl_qubit_is_one) {
+                int j = i ^ (1 << target_qubit_idx);
+                if (i < j) {
+                    Complex!real temp_i = this.state[i];
+                    Complex!real temp_j = this.state[j];
+                    this.state[i] = (temp_i + temp_j) / sqrt(2.0);
+                    this.state[j] = (temp_i - temp_j) / sqrt(2.0);
+                }
+            }
+        }
+    }
+
     /// The pauli-x gate or NOT gate, negates the current state of the qubit so |0> -> |1> and |1> -> |0>.
     /// More concisely it performs a bit flip. 
     void pauli_x(int qubit_idx) {
@@ -90,7 +108,6 @@ struct QuantumCircuit {
                         Complex!real(1, 0), Complex!real(0, 0)
                     ])
             ]);
-
         auto pairs = new Vector!int[(this.state.length() / 2)];
         int pairs_idx = 0;
 
@@ -136,6 +153,7 @@ struct QuantumCircuit {
 
     /// The controlled NOT gate checks if the control qubit is |1> if so it flips the target qubit.
     void cnot(int control_qubit_idx, int target_qubit_idx) {
+        assert(this.num_qubits >= 2, "The number of qubits must be greater than or equal to two in order to use controlled gates");
         for (int i = 0; i < this.state.length(); i++) {
             bool control_is_one = (i & (1 << control_qubit_idx)) != 0;
             if (control_is_one) {
@@ -172,6 +190,7 @@ struct QuantumCircuit {
     /// The controlled z gate applies a phase flip to the target qubit if both the 
     /// control and target are in the state |1>
     void cz(int control_qubit_idx, int target_qubit_idx) {
+        assert(this.num_qubits >= 2, "The number of qubits must be greater than or equal to two in order to use controlled gates");
         for (int i = 0; i < this.state.length(); i++) {
             bool cntl_qubit_is_one = (i & (1 << control_qubit_idx)) != 0;
             bool tgt_qubit_is_one = (i & (1 << target_qubit_idx)) != 0;
@@ -184,9 +203,11 @@ struct QuantumCircuit {
     /// The SWAP gate takes two qubits and if their states are different at index i it calculates a
     /// new position j to swap the amplitudes of two states.
     void swap(int qubit1, int qubit2) {
+        assert(this.num_qubits >= 2, "The number of qubits must be greater than or equal to two in order to use the swap gates");
         for (int i = 0; i < this.state.length(); i++) {
             int qubit1_val = (i >> qubit1) & 1;
-            int qubit2_val = (i >> qubit2) & 1;
+            int qubit2_val = (
+                i >> qubit2) & 1;
             if (qubit1_val != qubit2_val) {
                 int j = i ^ ((1 << qubit1) | (1 << qubit2));
                 if (i < j) {
@@ -201,6 +222,7 @@ struct QuantumCircuit {
     /// The ISWAP gate does the same thing as the SWAP gate but also multiplies the amplitudes
     /// of the states at index i and j by 0+1i
     void iswap(int qubit1, int qubit2) {
+        assert(this.num_qubits >= 2, "The number of qubits must be greater than or equal to two in order to use the swap gates");
         for (int i = 0; i < this.state.length(); i++) {
             int qubit1_val = (i >> qubit1) & 1;
             int qubit2_val = (i >> qubit2) & 1;
@@ -210,7 +232,6 @@ struct QuantumCircuit {
                     Complex!real temp = this.state[i];
                     this.state[i] = this.state[j];
                     this.state[j] = temp;
-
                     this.state[i] = this.state[i] * Complex!real(0, 1);
                     this.state[j] = this.state[j] * Complex!real(0, 1);
 
@@ -219,12 +240,25 @@ struct QuantumCircuit {
         }
     }
 
+    /// The CR_k gate or controlled rotation of order k gate, rotate the phase by e^PI / 2^(k-1)
+    /// if and only if the control and target qubits are 1
+    void cr(int control_qubit_idx, int target_qubit_idx, int k) {
+        assert(this.num_qubits >= 2, "The number of qubits must be greater than or equal to two in order to use the controlled rotation gate");
+        for (int i = 0; i < this.state.length(); i++) {
+            int cntl_qubit_val = (i >> control_qubit_idx) & 1;
+            int tgt_qubit_val = (i >> target_qubit_idx) & 1;
+
+            if (cntl_qubit_val == 1 && tgt_qubit_val == 1) {
+                this.state[i] = this.state[i] * expi(2 * PI / pow(2, k));
+            }
+        }
+    }
+
     /// Collapse the possible superposition of basis states into one classical state 
     /// based on inverse transform sampling (https://en.wikipedia.org/wiki/Inverse_transform_sampling)
     string measure() {
         Vector!float probs = Vector!float(cast(int) this.state.length(), new float[this
-                .state.length()]);
-        // Take the magnitude of each complex probability amplitude
+                .state.length()]); // Take the magnitude of each complex probability amplitude
         foreach (i, c; this.state.elems) {
             float magnitude = sqrt(pow(c.re, 2) + pow(c.im, 2));
             float prob = pow(magnitude, 2);
